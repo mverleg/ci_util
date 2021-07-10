47734bd6fb41c9d0cb9ccbd2ba3307d276b37465
Mark Verleg <mverleg.noreply@gmail.com>
2021-07-10 23:06:46 +0200

    Script to build Cargo dependencies only

---
+++ util/build_dependencies_only.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+set -eEu
+
+if [ ! -f Cargo.toml ]
+then
+    printf 'No Cargo.toml found\n' 1>&2
+    exit 1
+fi
+
+grep -E '^path\s*=\s*"([^"]*)"' Cargo.toml |
+    sed -E 's/.*"([^"]*)".*/\1/' |
+    xargs -I'$' sh -xc '
+        mkdir -p "$(dirname $)";
+        printf "\n// generated file to build dependencies\n#[allow(unused)]\nfn main() {}\n" > "$";
+        touch -t "200001010100" "$";'
+
+cargo build --workspace
+cargo build --workspace --release
+
+grep -E '^path\s*=\s*"([^"]*)"' Cargo.toml |
+    sed -E 's/.*"([^"]*)".*/\1/' |
+    xargs -I'$' sh -xc 'rm -f "$";'
+
+find . -name target -prune -o -type f
+printf 'done\n'
